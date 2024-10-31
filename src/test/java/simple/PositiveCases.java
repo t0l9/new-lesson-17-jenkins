fcfe5db889c0edbde2721abb3e891ff02c818204
t0l9 <t0l4ik.kas1@gmail.com>
2024-10-31 22:08:57 +0500

    add new

---
+++ src/test/java/simple/PositiveCases.java
@@ -0,0 +1,35 @@
+package simple;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple")
+public class PositiveCases {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test3() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test4() {
+        assertTrue(true);
+    }
+
+    @Test
+    void tes5() {
+        assertTrue(true);
+    }
+}
