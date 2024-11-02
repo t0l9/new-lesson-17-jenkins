508b807f46c7b4ad43512e7e298861450fb9c372
t0l9 <t0l4ik.kas1@gmail.com>
2024-11-02 16:49:37 +0500

    add annotates

--- src/test/java/simple/PositiveCases.java
+++ src/test/java/simple/PositiveCases.java
@@ -1,5 +1,10 @@
 package simple;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Tag("simple")
 public class PositiveCases {
 
+    @DisplayName("Первый автотест в jenkins")
+    @Tag("first_secusess")
+    @Owner("Kolyshkin")
+    @Severity(SeverityLevel.BLOCKER)
+    @Feature("Фича теста")
     @Test
     void test() {
         assertTrue(true);
